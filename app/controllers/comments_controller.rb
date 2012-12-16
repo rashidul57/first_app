d68f36dfd1672cbf54e64b5e70f468113a6c70c2
rashidul57 <mrashidbd2000@gmail.com>
2012-12-16 23:19:29 +0600

    First Commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,26 @@
+class CommentsController < ApplicationController
+  http_basic_authenticate_with :name => "a", :password => "123", :except => [:index, :show]
+  
+  def index
+    @comments = Comment.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @comments }
+    end
+  end
+  
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(params[:comment])
+    redirect_to post_path(@post)
+  end
+  
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+  
+end
---
+++ app/models/comment.rb
@@ -0,0 +1,6 @@
+class Comment < ActiveRecord::Base
+  attr_accessible :commenter, :body
+  belongs_to :post
+  
+  
+end
